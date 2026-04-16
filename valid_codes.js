acf9ed4fe84104e2f636fb6bf7bac7cd915f7249
87295480 <87295480@qq.com>
2026-04-17 06:47:59 +0800

    Add files via upload

---
+++ valid_codes.js
@@ -0,0 +1,86 @@
+/*在你的服务器上创建两个文件：
+
+/codes/valid_codes.json（解锁码数据）
+*/
+
+
+{
+  "phonetic_interactive_book": {
+    "DAYE-BOOK-SY3KII": {
+      "used": false,
+      "expireDate": "2026-05-16",
+      "bookTitle": "音标认知"
+    }
+  },
+  "phonetic_image_vowel": {
+    "DAYE-VOWEL-I0EEMD": {
+      "used": false,
+      "expireDate": "2026-05-16",
+      "bookTitle": "元音符号"
+    }
+  },
+  "phonetic_image_consonant": {
+    "DAYE-CONS-G2WB8N": {
+      "used": false,
+      "expireDate": "2026-05-16",
+      "bookTitle": "辅音符号"
+    }
+  },
+  "phonetic_image_all": {
+    "DAYE-ALLIMG-V9Q1XQ": {
+      "used": false,
+      "expireDate": "2026-05-16",
+      "bookTitle": "全部音标符号"
+    }
+  },
+  "phonetic_audio_vowel": {
+    "DAYE-AVOW-UBLO4E": {
+      "used": false,
+      "expireDate": "2026-05-16",
+      "bookTitle": "元音发音"
+    }
+  },
+  "phonetic_audio_consonant": {
+    "DAYE-ACONS-4PQBJ6": {
+      "used": false,
+      "expireDate": "2026-05-16",
+      "bookTitle": "辅音发音"
+    }
+  },
+  "phonetic_audio_all": {
+    "DAYE-ALLAUD-YPX8G5": {
+      "used": false,
+      "expireDate": "2026-05-16",
+      "bookTitle": "全部音标发音"
+    }
+  },
+  "phonetic_category": {
+    "DAYE-CATE-P2EGY1": {
+      "used": false,
+      "expireDate": "2026-05-16",
+      "bookTitle": "音标分类"
+    }
+  },
+  "spell_word_pronunciation": {
+    "DAYE-SPELL-UJBA0Q": {
+      "used": false,
+      "expireDate": "2026-05-16",
+      "bookTitle": "音标拼读"
+    }
+  },
+  "syllable_division": {
+    "DAYE-SYLL-UO4495": {
+      "used": false,
+      "expireDate": "2026-05-16",
+      "bookTitle": "音节划分"
+    }
+  },
+  "custom_001": {
+    "DAYE-CUSTOM1-H6C9XV": {
+      "used": false,
+      "expireDate": "2026-05-16",
+      "bookTitle": "李同学定制题库"
+    }
+  }
+}
+
